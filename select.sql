f298d473fca5346f1502c4adf1e71a2c280a2286
RichardYeoRepo <jeonghoun92@gmail.com>
2025-01-22 10:31:22 +0800

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,10 +1,14 @@
 -- Databricks notebook source
 USE CATALOG richard_yeo_demo;
 USE SCHEMA retail_richard_yeo;
-CREATE TABLE Persons (
+CREATE TABLE IF NOT EXISTS Persons (
     PersonID int,
     LastName varchar(255),
     FirstName varchar(255),
     Address varchar(255),
     City varchar(255)
 );
+INSERT INTO Persons
+    (PersonID, LastName, FirstName, Address, City)
+VALUES
+    (1,"Ceviche", "Tacos", "Flan", "City");
