b5b379bd1d5ed9d1cde90207c55efed468b66d93
RichardYeoRepo <jeonghoun92@gmail.com>
2025-01-22 10:26:35 +0800

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,2 +1,10 @@
 -- Databricks notebook source
-SELECT 1;
+USE CATALOG richard_yeo_demo;
+USE SCHEMA retail_richard_yeo;
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
